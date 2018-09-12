89907de1341214e263aaf0b5a3fe9447e1fc9fdb
Braden R <bradendrich@hotmail.com>
2018-09-12 15:34:28 -0600

    Character Controller

---
+++ 1610DGMFall2018/Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 MoveSpeed;
+    public Vector3 RotateSpeed;
+    public float speed;
+    public CharacterController Character;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MoveSpeed.x = speed * Input.GetAxis("Horizontal");
+        MoveSpeed *= Time.deltaTime;
+
+        Character.Move(MoveSpeed);
+	}
+}
