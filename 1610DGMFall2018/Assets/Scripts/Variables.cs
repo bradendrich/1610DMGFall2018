98301307f05d0da45f4bb39a6bb872eb268f6888
Braden R <bradendrich@hotmail.com>
2018-09-17 15:48:53 -0600

    Variables
    AddMoreMoney

---
+++ 1610DGMFall2018/Assets/Scripts/Variables.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // example of bools
+    public bool IsAWizard;
+
+    // example of integers
+    public int AmmoCount = 100;
+    public int InventoryCount;
+
+    // example of floats
+    public float Health = 1.0f;
+    public float Money;
+
+    //example of strings
+    public string PlayerName;
+    public string Team = "Wolves";
+
+    public Vector3 Location;
+
+	// Use this for initialization
+	void Start ()
+    {
+        print(PlayerName);
+        print("has " + Money + " money");
+
+        if (IsAWizard)
+        {
+            print(PlayerName + " Is a wizard");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (IsAWizard)
+        {
+            transform.position = Location;
+        }
+	}
+}
