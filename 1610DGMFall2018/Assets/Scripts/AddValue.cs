4f822a555888100980cfd1029f17f440cfa40187
Braden R <bradendrich@hotmail.com>
2018-09-19 15:40:37 -0600

    Generic Event Calling

---
+++ 1610DGMFall2018/Assets/Scripts/AddValue.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AddValue : ScriptableObject
+{
+    public FloatData ValueObj;
+    public FloatData MaxValue;
+    public FloatData MinValue;
+
+    public UnityEvent EventMax;
+    public UnityEvent EventMin;
+
+    public void AddValueToObj(FloatData data)
+    {
+        ValueObj.Value += data.Value;
+
+        if(ValueObj.Value <= 0)
+        {
+            EventMin.Invoke();
+        }
+
+        if (ValueObj.Value <= MinValue.Value)
+        {
+            EventMin.Invoke();
+        }
+    }
+}
---
+++ 1610DGMFall2018/Assets/Scripts/MouseDownBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class MouseDownBehaviour : MonoBehaviour
+{
+
+    public UnityEvent Event;
+
+    private void OnMouseDown()
+    {
+        Event.Invoke();   
+    }
+}
