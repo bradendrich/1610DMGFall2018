f672c4bf03296e0b6356d380368f4ccb9ba71e2e
Braden R <bradendrich@hotmail.com>
2018-09-26 15:44:25 -0600

    Add Value

---
+++ 1610DGMFall2018/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class PowerUp : ScriptableObject
+{
+    public FloatData Data;
+
+    public void OnPowerUP (FloatData outsideData)
+    {
+        outsideData.Value += Data.Value;
+    }
+}
---
+++ 1610DGMFall2018/Assets/Scripts/TriggerEventBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class TriggerEventBehaviour : MonoBehaviour
+{
+    public UnityEvent Enable;
+    public UnityEvent Trigger;
+
+    private void OnEnable()
+    {
+        Enable.Invoke();
+    }
+
+    private void OnTriggerEnter()
+    {
+        Trigger.Invoke();
+    }
+}
