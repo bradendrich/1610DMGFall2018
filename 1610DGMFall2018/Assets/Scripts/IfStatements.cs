b6ae782116ef63a0f004f0182afbf0aaa55115e5
Braden R <bradendrich@hotmail.com>
2018-10-01 15:36:01 -0600

    NameID

--- 1610DGMFall2018/Assets/Scripts/IfStatements.cs
+++ 1610DGMFall2018/Assets/Scripts/IfStatements.cs
@@ -8,30 +8,42 @@ public class IfStatements : MonoBehaviour
     public string Password;
     public int A, B, C;
 
+    public NameID Lock, Key;
+
+    public FloatData WizardPower, PawnPower;
+
 	void Update () {
+        if(Lock == Key)
+        {
+            print("Open Door");
+        }
+
+        if (WizardPower.Value > PawnPower.Value)
+        {
+            print("The Wizard Wins");
+        }
+
         if (LightOn)
         {
             print("The light is on.");
+        }
+        else
+        {
+            print("The light is off");
         }
 
         if (Password == "OU812")
         {
             print("The Password is correct");
         }
+        else
+        {
+            print("The password is incorrect");
+        }
 
         if(C == A + B)
         {
             print(C);
-        }
-
-        if (Password != "OU812")
-        {
-            print("The password is not correct");
-        }
-
-        if (!LightOn)
-        {
-            print("The Light is Off");
         }
 	}
 }
