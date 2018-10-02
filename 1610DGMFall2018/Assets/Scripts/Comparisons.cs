4b13a34770dc9b82156ee62a85d5066d79b61b4b
Braden R <bradendrich@hotmail.com>
2018-10-02 17:23:37 -0600

    OnComparisons

---
+++ 1610DGMFall2018/Assets/Scripts/Comparisons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comparisons : MonoBehaviour
+{
+    public FloatData DefaultValue;
+
+    public void OnComparisonsValue(FloatData data)
+    {
+        if (DefaultValue.Value == data.Value)
+        {
+            print("You may pass");
+        }
+        else if (DefaultValue.Value < data.Value)
+        {
+            print("You may skip this area");
+        }
+        else
+        {
+            print("You Shall not pass!!!");
+        }
+    }
+}
