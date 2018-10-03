0678cc3a48d106d873ee0d75c4a7a9253b00f70c
Braden R <bradendrich@hotmail.com>
2018-10-03 15:46:14 -0600

    Character Controller

---
+++ 1610DGMFall2018/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float Gravity = 9.81f;
+    public float MoveSpeed = 3.0f;
+    public float JumpSpeed = 3.00f;
+    private Vector3 position;
+    private Vector3 rotation;
+
+	void Start ()
+    {
+        controller = GetComponent<CharacterController>();	
+	}
+	
+	void Update ()
+    {
+        rotation.Set(0, Input.GetAxis("Horizontal"), 0);
+        transform.Rotate(rotation);
+        if (controller.isGrounded)
+        {
+            position.Set(0, 0, MoveSpeed * Input.GetAxis("Vertical"));
+            position = transform.TransformDirection(position);
+
+            if (Input.GetButton("Jump"))
+            {
+                position.y = JumpSpeed;
+            }
+        }
+
+        position.y -= Gravity * Time.deltaTime;
+        controller.Move(position * Time.deltaTime);
+	}
+}
