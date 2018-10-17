ef094cc596d1784e124f98440a846ba72d233c77
Braden R <bradendrich@hotmail.com>
2018-10-17 15:33:54 -0600

    Health Bar

---
+++ 1610DGMFall2018/Assets/Scripts/ChangeValue.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ChangeValue : ScriptableObject
+{
+    public FloatData Data;
+
+    public void AddValueToObj(FloatData data)
+    {
+        Data.Value += data.Value;
+    }
+
+    public void SubtractValueToObj (FloatData data)
+    {
+        Data.Value -= data.Value;
+    }
+
+    public void MultiplyValueToObj (FloatData data)
+    {
+        Data.Value *= data.Value;
+    }
+
+    public void DivideValueToObj (FloatData data)
+    {
+        Data.Value /= data.Value;
+    }
+}
---
+++ 1610DGMFall2018/Assets/Scripts/UpdateImageValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UpdateImageValue : MonoBehaviour
+{
+    public FloatData Data;
+    private Image image;
+
+	void Start ()
+    {
+        image = GetComponent<Image>();
+	}
+	
+	void Update ()
+    {
+        image.fillAmount = Data.Value;
+	}
+}
