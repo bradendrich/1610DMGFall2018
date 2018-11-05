ffe62e0b939b6157c1eb82d4d4b44003a48063c5
Braden R <bradendrich@hotmail.com>
2018-11-05 15:47:25 -0700

    AI

---
+++ 1610DGMFall2018/Assets/Scripts/BasicAI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class BasicAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform Destination;
+
+    void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+    void Update ()
+    {
+        if (Destination != null) agent.destination = Destination.position;
+	}
+}
