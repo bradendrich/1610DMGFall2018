89c1d5a5160eaf3b2d91fc8c207ab0a761b63c18
Braden R <bradendrich@hotmail.com>
2018-11-30 14:32:38 -0700

    Respawn not working

---
+++ 1610DGMFall2018/Assets/Scripts/Respawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject SpawnPoint;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Player == null) Instantiate(Player, SpawnPoint.transform.position, SpawnPoint.transform.rotation);
+    } 
+}
