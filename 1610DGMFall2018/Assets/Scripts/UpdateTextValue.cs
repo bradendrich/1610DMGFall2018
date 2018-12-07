3532be4629b38264f5bc6230f02d99db9cbf8bfe
Braden R <bradendrich@hotmail.com>
2018-12-06 17:24:08 -0700

    Score Keeping
    UpdateTextValue

---
+++ 1610DGMFall2018/Assets/Scripts/UpdateTextValue.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class UpdateTextValue : MonoBehaviour
+{
+    public Text Value;
+    public FloatData Score;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+        Value.text = Score.Value.ToString();
+	}
+}
