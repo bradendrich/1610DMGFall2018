f9de2b4880f6780bff3e2c912452d8ed273b2c0c
Braden R <bradendrich@hotmail.com>
2018-12-06 18:23:47 -0700

    Scoring

--- 1610DGMFall2018/Assets/Scripts/BasicAI.cs
+++ 1610DGMFall2018/Assets/Scripts/BasicAI.cs
@@ -7,6 +7,7 @@ public class BasicAI : MonoBehaviour
     private NavMeshAgent agent;
     public Transform Destination;
     public GameAction Action;
+    private bool isFlipped;
 
     void Start ()
     {
@@ -17,6 +18,16 @@ public class BasicAI : MonoBehaviour
     void Update ()
     {
         if (Destination != null) agent.destination = Destination.position;
+        if (agent.velocity.x >= 0 && isFlipped == false)
+        {
+            transform.Rotate(0, 180, 0);
+            isFlipped = true;
+        }
+        else if (agent.velocity.x <= 0 && isFlipped == true)
+        {
+            transform.Rotate(0, 180, 0);
+            isFlipped = false;
+        }
     }
 
     public void UpdateDestination(object location)
